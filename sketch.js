7d5044e8ff4a9312b71de00f39fff15356e5ec0a
ParanjoyG <43812798+ParanjoyG@users.noreply.github.com>
2020-10-31 20:09:05 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,54 +1,47 @@
-let input, button, greeting;
-var name = 'Anusmitaa';
-var font;
-var vehicles = [];
-
-function preload() {
-  font = loadFont('AvenirNextLTPro-Demi.otf');
-}
+let x = 0;
+let y = 0;
 
 function setup() {
-  createCanvas(1450, 500);
-  background(51);
-  input = createInput();
-  input.position(20, 65);
-  fill(255);
-
-  button = createButton('submit');
-  button.position(input.x + input.width, 65);
-  button.mousePressed(change);
-
-  greeting = createElement('h2', 'Enter the Word.');
-  greeting.position(20, 5);
-
-  textAlign(CENTER);
-  textSize(50);
-  
-  
- 
-  
+  createCanvas(600, 600);
+  background(0);
 }
 
-function change()
-{
-  name = input.value();
-  vehicles = [];
-  var points = font.textToPoints(name, 100, 300, 192, {
-    sampleFactor: 0.25
-  });
+function drawPoint() {
+  colorMode(HSB, 255, 255, 255);
+  stroke(map(y, 0, 9.9983, 0, 255), 255, 255, 200);
+  strokeWeight(1);
+  let px = map(x, -2.1820, 2.6558, 0, width);
+  let py = map(y, 0, 9.9983, height, 0);
+  point(px, py);
+}
 
-  for (var i = 0; i < points.length; i++) {
-    var pt = points[i];
-    var vehicle = new Vehicle(pt.x, pt.y);
-    vehicles.push(vehicle);
+function nextPoint() {
+  let nextX;
+  let nextY;
+
+  let r = random(1);
+
+  if (r < 0.01) {
+    nextX = 0;
+    nextY = 0.16 * y;
+  } else if (r < 0.86) {
+    nextX = 0.85 * x + 0.04 * y;
+    nextY = -0.04 * x + 0.85 * y + 1.60;
+  } else if (r < 0.93) {
+    nextX = 0.20 * x + -0.26 * y;
+    nextY = 0.23 * x + 0.22 * y + 1.60;
+  } else {
+    nextX = -0.15 * x + 0.28 * y;
+    nextY = 0.26 * x + 0.24 * y + 0.44;
   }
+
+  x = nextX;
+  y = nextY;
 }
+
 function draw() {
-  background(101);
-  for (var i = 0; i < vehicles.length; i++) {
-    var v = vehicles[i];
-    v.behaviors();
-    v.update();
-    v.show();
+  for (let i = 0; i < 1000; i++) {
+    drawPoint();
+    nextPoint();
   }
 }
